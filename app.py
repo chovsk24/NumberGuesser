2425425f713e34b170be1cfcc3ebd96c530cb712
Alexey Chechkovsky <61628572+chovsk24@users.noreply.github.com>
2021-06-08 12:13:29 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,24 @@
 #nuber guesser
-import time
 import random
 
 gameRun = True
-guessEngine = True
 score = "0" 
 
-while gameRun == True:
-    randomNumber = (random.randint(1, 10))
-    
-    print("I am thinking of a number from 1 through 10!\n")
-    print("Can you guess it?\n")
+print("I am thinking of a number from 1 through 100!\n")
+print("If it's too high, or too low I will tell you.\n")
+print("Can you guess it?\n")
 
+while gameRun == True:
+    randomNumber = (random.randint(1, 100))
+    
     guessNumber = input(":")
     guessNumber = int(guessNumber)
     
+    if guessNumber < randomNumber:
+        print("Too Low!")
+        
+    if guessNumber > randomNumber:
+        print("Too High!")
+    
     if guessNumber == randomNumber:
-        print("You Won!\n")
-    else:
-        print("You Lost!\n")
-        time.sleep(.5)
-        
-        
-        
-        
+        print("You Won!")
