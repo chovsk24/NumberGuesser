47c549a6c28cc4d8af1567b48c0b6ae0feb75539
Alexey Chechkovsky <61628572+chovsk24@users.noreply.github.com>
2021-06-09 09:41:10 -0400

    Added score system.

--- app.py
+++ app.py
@@ -1,24 +1,25 @@
 #nuber guesser
 import random
 
-gameRun = True
-score = "0" 
+score = 0 
 
-print("I am thinking of a number from 1 through 100!\n")
-print("If it's too high, or too low I will tell you.\n")
-print("Can you guess it?\n")
+print("I am thinking of a number from 1 through 100!\n\nIf it's too high, or too low I will tell you.\n\nCan you guess it?\n")
 
-while gameRun == True:
-    randomNumber = (random.randint(1, 100))
+randomNumber = (random.randint(1, 100))
+
+while True:
     
     guessNumber = input(":")
     guessNumber = int(guessNumber)
     
     if guessNumber < randomNumber:
         print("Too Low!")
-        
+        score = score + 1
     if guessNumber > randomNumber:
         print("Too High!")
-    
+        score = score + 1
     if guessNumber == randomNumber:
-        print("You Won!")
+        print("You Won!\n")
+        score = str(score)
+        print("It took you " + score + " attemps! God you're pathetic.\n\n")
+        
